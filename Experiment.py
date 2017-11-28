d5932357302ff964606ca4d219730db45891748f
anwar79melb <anwar79du@gmail.com>
2017-11-28 13:47:51 +1100

    I am starting my rap
    This my git learning

---
+++ Experiment.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 17:02:06 2017
+
+@author: anwar
+"""
+
+import pandas as pd
+d = {'First' : [1., 2., 3.],'Second' : [1,2,3]}
+
+
+AA = pd.DataFrame(d)
+print(AA)
+
+# new line comment
